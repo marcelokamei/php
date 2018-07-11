ac20fd573e98af1e6c8cf33278eb31b8f57a559e
marcelokamei <dinamicapvh@gmail.com>
2018-07-11 17:01:19 -0400

    repositorio criado

---
+++ alomundo.php
@@ -0,0 +1,7 @@
+<?php
+
+    echo "<h1>Alou mundo PHP! Vamos começar com essa zorra toda.</h1><br>";
+    
+    echo "<h2>Eu acho que vai ser melhor para tudo mundo, especialmente pras contas.</h2>";
+
+?>
