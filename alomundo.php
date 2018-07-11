89cc86ef3160bdd63a5b7419f055dc9a2884871a
marcelokamei <31637209+marcelokamei@users.noreply.github.com>
2018-07-11 17:29:08 -0400

    Update alomundo.php

--- alomundo.php
+++ alomundo.php
@@ -1,7 +1,7 @@
 <?php
 
-    echo "<h1>Alou mundo PHP! Vamos começar com essa zorra toda.</h1><br>";
-    
+    echo "<h1>Alou mundo PHP! Vamos começar com essa zorra toda.</h1><br>";    
     echo "<h2>Eu acho que vai ser melhor para tudo mundo, especialmente pras contas.</h2>";
+    echo "<h3>A alteração nessa linha foi feita com o GitHub, pelo site.</h3>";
 
 ?>
