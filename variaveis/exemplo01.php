095b0633d94e5c7b94c3f81e3f4d9e9664560daa
marcelokamei <marcelokamei@hotmail.com>
2018-07-11 23:08:20 -0400

    estudando variaveis em php

---
+++ variaveis/exemplo01.php
@@ -0,0 +1,11 @@
+<?
+
+    $nome = "Marcelo Kamei";
+    $num1 = 12;
+    $num2 = 3;
+
+    $soma = $num1 + $num2;
+
+    echo "<h4>Olá ".$nome.", a soma dos números contidos nas variáveis é: ".$soma."</h4>";
+
+?>
