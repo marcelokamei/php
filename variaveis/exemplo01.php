b5a6bd1554a1b431f5d369784bb3f240576a7257
marcelokamei <marcelokamei@hotmail.com>
2018-07-11 23:20:30 -0400

    comando ISSET e UNSET

--- variaveis/exemplo01.php
+++ variaveis/exemplo01.php
@@ -1,5 +1,6 @@
 <?
 
+    /*
     $nome = "Marcelo Kamei";
     $num1 = 12;
     $num2 = 3;
@@ -7,5 +8,22 @@
     $soma = $num1 + $num2;
 
     echo "<h4>Olá ".$nome.", a soma dos números contidos nas variáveis é: ".$soma."</h4>";
+    */
+
+    $nome = "Marcelo";
+    $sobrenome = "Menezes Kamei";
+    $nomeCompleto = $nome." ".$sobrenome;
+    
+    echo "<h2>".$nomeCompleto."</h2>";
+    echo "<br/>";
+    
+    if(isset($nome))
+        echo "Nome: ".$nome;
+        echo "<br/>";
+
+    if(isset($sobrenome))
+        echo "Sobrenome: ".$sobrenome;
+
+    unset($nome, $sobrenome, $nomeCompleto);
 
 ?>
